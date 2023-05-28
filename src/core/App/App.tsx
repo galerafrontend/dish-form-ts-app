7e5a23f8d68c51bf57a5a991a7dc341f9e9eeadf
Karol Galera <frontend.galera@gmail.com>
2023-05-29 00:12:30 +0200

    App with Layout and Section added

---
+++ src/core/App/App.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "../../components/Layout/styled";
+import Section from "../../components/Section";
+
+const App = () => {
+  return (
+    <Layout>
+      <Section />
+    </Layout>
+  );
+};
+
+export default App;
